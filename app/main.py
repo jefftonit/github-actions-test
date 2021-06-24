57f7887659f57c9994556d03982073be3cee52cb
Jeff Harris <jeff@tonit.com>
2021-06-24 16:48:30 -0700

    inject secret string

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import logging
+import os
 logger = logging.getLogger(__name__)
 
 
 
 async def app(scope, receive, send):
     logger.debug('log my app')
+    tester = os.getenv('STYLE_PILOT_CONN_STR')
+    logger.debug('test var: %s', tester)
     assert scope['type'] == 'http'
 
     await send({
