4552530f067385bd30f8b6b27c19580039d25b7f
Jeff Harris <jeff@tonit.com>
2021-06-24 11:02:47 -0700

    entry update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,20 @@
 import logging
 logger = logging.getLogger(__name__)
-logger.debug('log my app')
-print("my app")
+
+
+
+async def app(scope, receive, send):
+    logger.debug('log my app')
+    assert scope['type'] == 'http'
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, world!',
+    })
